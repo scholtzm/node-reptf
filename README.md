db0db6615dbdfd7a222778884849a3e65d88bd68
Michael Scholtz <michael.scholtz@outlook.com>
2015-07-25 13:42:46 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,49 @@
+# rep.tf API Wrapper for node.js
+
+[![NPM version](http://img.shields.io/npm/v/reptf.svg?style=flat)](https://www.npmjs.org/package/reptf)
+[![Dependency Status](https://david-dm.org/scholtzm/node-reptf.svg)](https://david-dm.org/scholtzm/node-reptf)
+
+Very thin wrapper for the API provided by [rep.tf](http://rep.tf).
+
+### Installation
+
+`npm install reptf`
+
+### Usage
+
+```js
+var reptf = require('reptf');
+
+// All methods are "static"
+reptf.getBans('76561197960435530', function(err, res) {
+    if(!err) {
+        console.log(res);
+    }
+});
+
+reptf.getProfile('76561197960435530', function(err, res) {
+    if(!err) {
+        console.log(res);
+    }
+});
+```
+
+### Methods
+
+##### getBans(steamID, callback)
+
+- `steamID` - user's SteamID64 as string
+- `callback` - should be `function(error, result)`
+	- `error` comes from the HTTP request
+	- `result` is object returned by the API
+
+##### getProfile(steamID, callback)
+
+- `steamID` - user's SteamID64 as string
+- `callback` - should be `function(error, result)`
+	- `error` comes from the HTTP request
+	- `result` is object returned by the API
+
+### License
+
+MIT. See `LICENSE`.
---
+++ index.js
@@ -0,0 +1,60 @@
+var request = require('request');
+
+var timeout = 10000; // 10s, hardcoded for now
+
+// Main API object
+var repTFAPI = {
+    _BANS_URL: "http://rep.tf/api/bans?str=",
+    _PROFILE_URL: "http://rep.tf/api/profile?str="
+};
+
+/**
+ * Returns users's list of bans.
+ * @param {string}   steamID  User's SteamID64 as string.
+ * @param {function} callback Callback function.
+ */
+repTFAPI.getBans = function(steamID, callback) {
+    var finalUrl = this._BANS_URL + steamID;
+
+    var options = {
+        url: finalUrl,
+        method: "POST",
+        timeout: timeout,
+        json: true
+    };
+
+    request(options, function(error, response, body) {
+        if(!error && response.statusCode === 200) {
+            callback(null, body);
+        } else {
+            callback(error, null);
+        }
+    });
+};
+
+/**
+ * Returns users's profile.
+ * @param {string}   steamID  User's SteamID64 as string.
+ * @param {function} callback Callback function.
+ */
+repTFAPI.getProfile = function(steamID, callback) {
+    var finalUrl = this._PROFILE_URL + steamID;
+
+    var options = {
+        url: finalUrl,
+        method: "POST",
+        timeout: timeout,
+        json: true
+    };
+
+    request(options, function(error, response, body) {
+        if(!error && response.statusCode === 200) {
+            callback(null, body);
+        } else {
+            callback(error, null);
+        }
+    });
+};
+
+// Export API object
+module.exports = repTFAPI;
